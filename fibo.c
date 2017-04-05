ea5ee0525237d6c5747d14c5bbc3f5cfd1e7e808
danielgomez2501 <david.crespo@correounivalle.edu.co>
2017-04-05 16:02:41 -0500

    first commit

---
+++ fibo.c
@@ -0,0 +1,27 @@
+
+int fibo()
+int num()
+
+int main(){
+
+int fibo(n){
+
+printf("Digite un número");
+scanf("%d, &num");
+
+if num==1
+return 1;
+
+
+if num==2
+fibo=1;
+
+
+else
+fibo=fibo(num-1)+fibo(num-1);
+
+
+return fibo
+}
+}
+
---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "fibo.h"
+
+int fibo()
+int num()
+
+int main(){
+
+int fibo(n){
+
+printf("Digite un número");
+scanf("%d, &num");
+
+if num==1
+return 1;
+
+
+if num==2
+fibo=1;
+
+
+else
+fibo=fibo(num-1)+fibo(num-1);
+
+
+return fibo
+}
+}
+
